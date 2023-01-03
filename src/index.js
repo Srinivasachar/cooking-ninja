6cce735ba07ed022129bacbb7040e0eb7494d2a1
Srinivas S <srinivas.s@sap.com>
2023-01-03 17:36:29 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ThemeProvider } from './context/ThemeContext';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+//Before-> Change state > rerender
+//useEffect -> first function, params
